5aeb2577c820f6ebcecbc92f854e062ab95ff54c
val <izol@rambler.ru>
2024-05-03 15:52:11 +0300

    С assert был косячек :)

--- test_items.py
+++ test_items.py
@@ -5,4 +5,4 @@ link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/."
 
 def test_contains_an_add_button(browser):
     browser.get(link)
-    assert browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket").is_displayed, "there is no add to cart button"
+    assert len(browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")) == 1, "there is no add to cart button"
